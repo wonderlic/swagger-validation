70a7639e710f2e5e19d055cb8b044d81ee39b7ac
Chris Katz <chris.katz@wonderlic.com>
2014-07-30 15:44:29 -0500

    adding in docs

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ gulp.task('jsdoc', function() {
   gulp.src('').pipe(shell(['"./node_modules/.bin/jsdoc" ./lib -r -t ./node_modules/ink-docstrap/template -c ./jsdoc.conf.json -d ./docs/jsdoc']));
 });
 
-// watch for any changes in the lib directory and re-run docker and jsdoc tasks to make the documentation
+// watch for any changes in the lib directory and re-run  jsdoc tasks to make the documentation
 gulp.task('watch', function() {
   gulp.watch('./lib/**/*.js', ['jsdoc']);
 });
