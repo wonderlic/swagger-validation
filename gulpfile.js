a40a4b51284b6353d26b76a2c535f44c4be90893
Chris Katz <chris.katz@wonderlic.com>
2014-07-30 09:17:04 -0500

    removing docker and updating packages

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,6 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 
-// run docker task
-gulp.task('docker', function() {
-  gulp.src('').pipe(shell(['"./node_modules/.bin/docker" -i ./lib -o ./docs/docker -c native -n']));
-});
-
 // run jsdoc task
 gulp.task('jsdoc', function() {
   gulp.src('').pipe(shell(['"./node_modules/.bin/jsdoc" ./lib -r -t ./node_modules/ink-docstrap/template -c ./jsdoc.conf.json -d ./docs/jsdoc']));
@@ -15,7 +10,7 @@ gulp.task('jsdoc', function() {
 
 // watch for any changes in the lib directory and re-run docker and jsdoc tasks to make the documentation
 gulp.task('watch', function() {
-  gulp.watch('./lib/**/*.js', ['jsdoc', 'docker']);
+  gulp.watch('./lib/**/*.js', ['jsdoc']);
 });
 
-gulp.task('default', ['docker', 'jsdoc', 'watch']);
+gulp.task('default', ['jsdoc', 'watch']);
